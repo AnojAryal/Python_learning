23545a44117045d5da6ef335843d6001c5cdc081
AnojAryal <anoj1810@gmail.com>
2023-03-13 21:11:51 +0545

    first commit

---
+++ First.py
@@ -0,0 +1,21 @@
+a = 5
+b = 10
+c = a+b
+print(c)
+
+print("anoj is a good boy ")
+
+print(10+(5-2)*3/1)   #float value
+
+print(10+(5-2)*3//1)   #integer
+
+print("anoj"+"anoj")  #concat
+
+print("anoj"*10)
+
+print(r"c:/anoj/navin")
+
+print("this is anoj's myth")   #raw element
+
+print('this is franzy\'s world')  
+
