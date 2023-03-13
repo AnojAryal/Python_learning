8e6e7053d3272588dd100b71f2e3460a4900bc66
AnojAryal <anoj1810@gmail.com>
2023-03-13 22:06:06 +0545

    variables

---
+++ Variables.py
@@ -0,0 +1,23 @@
+x=2
+y=3
+z=x+y
+print(z)
+
+print(x+10)
+
+name = 'franzy'
+print (name)
+
+c=name+" "+"rocks"
+
+print(c)
+
+print(name[0])
+print(name[4])
+print(name[1-4])
+print(name[1:])
+print(name[:4])
+print(len(name))
+print('my'+name)
+
+
