877afda33b32cc95dfbda30460277abeb26f0d1e
AnojAryal <anoj1810@gmail.com>
2023-03-14 22:40:04 +0545

    tuples

---
+++ Tuple.py
@@ -0,0 +1,42 @@
+tuple=(11,22,33,44,55,66,77)
+
+print(tuple)
+print('----------------------')
+print(tuple[3])
+
+#working with sets
+print('----------------------')
+sets={22,44,55,67,98,13,98}
+print(sets)
+print('----------------------')
+#working with variables
+
+num=10
+print(id(num))   #prints the address of 10
+print('----------------------')
+name = "franzy"
+print(id(name))  #prints the address of name
+print('----------------------')
+a=20
+b=a
+
+print(id(a))
+print(id(b))
+print('----------------------')
+"""in python when we create a multiple variables 
+having same data they both will point a same box"""
+
+print(id(20))
+print(id(10))
+print('----------------------')
+a=9
+b=a
+print(id(a))
+print(id(b))
+print('----------------------')
+print(type(a))
+r="ram"
+print(type(r))
+
+
+
