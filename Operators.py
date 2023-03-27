eb3e313ea26585b888e058ce83e34188615759f7
AnojAryal <anoj1810@gmail.com>
2023-03-27 10:24:03 +0545

    operators in python

---
+++ Operators.py
@@ -0,0 +1,35 @@
+# Addition
+a = 5
+b = 7
+c = a + b
+print("The sum of", a, "and", b, "is", c)
+
+# Subtraction
+d = 12
+e = 5
+f = d - e
+print("The difference between", d, "and", e, "is", f)
+
+# Multiplication
+g = 6
+h = 4
+i = g * h
+print("The product of", g, "and", h, "is", i)
+
+# Division
+j = 20
+k = 4
+l = j / k
+print("The quotient of", j, "and", k, "is", l)
+
+# Modulus
+m = 17
+n = 3
+o = m % n
+print("The remainder when", m, "is divided by", n, "is", o)
+
+# Exponentiation
+p = 2
+q = 3
+r = p ** q
+print(p, "raised to the power of", q, "is", r)
