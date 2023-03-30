12abb6914ca4c7027c6b7bff26d4abbd42516d83
AnojAryal <anoj1810@gmail.com>
2023-03-30 08:53:19 +0545

    in range in python

---
+++ inRange.py
@@ -0,0 +1,13 @@
+# Python program using range()
+
+# Define a range of numbers from 0 to 9 (inclusive)
+for i in range(10):
+    print(i)
+
+# Define a range of numbers from 1 to 10 (inclusive)
+for j in range(1, 11):
+    print(j)
+
+# Define a range of numbers from 0 to 100 (inclusive) in increments of 10
+for k in range(0, 101, 10):
+    print(k)
