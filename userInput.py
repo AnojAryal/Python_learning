a551feb6532e0ae363c5f9b8b3f1e7ea34980e65
AnojAryal <anoj1810@gmail.com>
2023-04-23 17:50:04 +0545

    user input in python

---
+++ userInput.py
@@ -0,0 +1,11 @@
+x = int(input("Enter the first number :"))
+y = int(input("Enter the second number :"))
+
+z = x+y
+
+print("the sum of 2 numbers is :",z)
+
+
+result = eval(input('Enter the eval :'))
+
+print(result)
