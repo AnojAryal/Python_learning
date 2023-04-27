30ae301408fa6658c8d2e6c9f44398ae15197349
AnojAryal <anoj1810@gmail.com>
2023-04-27 12:41:46 +0545

    exception handling in python

---
+++ exceptionHandling.py
@@ -0,0 +1,8 @@
+try:
+    x = int(input("Enter a number: "))
+    y = 10 / x
+    print("Result: ", y)
+except ValueError:
+    print("Invalid input. Please enter a valid number.")
+except ZeroDivisionError:
+    print("Cannot divide by zero.")
